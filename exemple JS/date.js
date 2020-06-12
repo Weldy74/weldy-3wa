45393a1ea53336e8810d4c4cb00fc1bb451057ec
Weldy74 <goubet.jr@hotmail.fr>
2020-06-12 14:29:26 +0000

    première sauvegarde

---
+++ exemple JS/date.js
@@ -0,0 +1,23 @@
+
+const aujourdhui = new Date();
+
+const day = aujourdhui.getDay();
+
+const jour = aujourdhui.getDate();
+
+const mouth = aujourdhui.getMonth();
+
+const year = aujourdhui.getFullYear();
+
+// sunday - saturday : 0 - 6
+
+const days = ["dimanche", "lundi","mardi", "mercredi", "jeudi", "vendredi", "samedi"] ;
+
+// January - December : 0 - 11
+
+const mouths = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
+
+console.log(mouths[mouth], days[day], jour, year);
+
+document.write("nous sommes le"  ++ day ++ mouth  ++ year);
+
